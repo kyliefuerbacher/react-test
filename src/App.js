e1bcc420b4615090c104aedb54b0add25947a98d
Ttargetarc <137192962+Ttargetarc@users.noreply.github.com>
2023-07-27 09:11:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import DataType from './components/DataType';
-import SourceType from './components/SourceType';
-import OpenFile from './components/OpenFile';
-  
-export default function App() {
-  
-  return (
-    <>
-    <DataType/>
-    <SourceType/>
-    <OpenFile/>
-    </>
-  );
-}
+// backend/app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+const MONGODB_URI = 'mongodb://localhost:27017/mydatabase'; // Replace 'mydatabase' with your MongoDB database name
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+// Define a sample data collection schema
+const sampleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+const SampleModel = mongoose.model('Sample', sampleSchema);
+
+// CORS middleware
+app.use(cors());
+
+// Fetch data from MongoDB
+app.get('/api/data', async (req, res) => {
+  try {
+    const data = await SampleModel.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching data from MongoDB' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
